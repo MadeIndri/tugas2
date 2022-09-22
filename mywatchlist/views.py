aa2eee2a4b38f5cbe698056de745f228fe5b52ab
MadeIndri07 <madeindrim@gmail.com>
2022-09-22 10:50:28 +0700

    add mywatchlist

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -2,14 +2,31 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render
 from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
 
 # Create your views here.
+
 def show_mywatchlist(request):
     data_film_mywatchlist = MyWatchList.objects.all()
+    watched = 0
+    unwatched = 0
+    output = " "
+
+    for film in data_film_mywatchlist:
+        if (film.watched == True):
+            watched += 1
+        else:
+            unwatched += 1
+    
+    if (watched >= unwatched):
+        output = "Selamat, kamu sudah banyak menonton!"
+    else:
+        output = "Wah, kamu masih sedikit menonton!"
     context = {
         'list_film': data_film_mywatchlist,
         'nama': 'Made Indri Maharani Natiadewi',
-        'id': '2106704295'
+        'id': '2106704295',
+        'output': output
     }
     return render(request, "mywatchlist.html", context)
 
